4b49281a5b745037bc46dc3864573fcbf2df9a18
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-07-30 00:23:50 +0530

    Refactor:- refactored for more than 3 parameter.

--- src/com/Bridgelabz/generics/GenericsFindMax.java
+++ src/com/Bridgelabz/generics/GenericsFindMax.java
@@ -1,19 +1,25 @@
 package com.Bridgelabz.generics;
 
+import java.util.*;
+
 public class GenericsFindMax <T extends Comparable<T>> {
 
-    public <T extends Comparable<T>>T findMaximum(T x,T y,T z){
-        T max = x;
-        if (y.compareTo(max)>0)
-            max = y;
-        if (z.compareTo(max)>0)
-            max = z;
+    public <T extends Comparable<T>>T findMaximum(T... x){
+        T max = x[0];
+        List <T> parameterArray = new ArrayList<>();
+        for (T element : x){
+            parameterArray.add(element);
+            if(element.compareTo(max)>0){
+                max = element;
+            }
+        }
+        System.out.println("Maximum of "+parameterArray.toString()+" is "+max);
         return max;
     }
     public static void main(String[] args) {
 	GenericsFindMax genericsFindMax = new GenericsFindMax();
-        System.out.println(genericsFindMax.findMaximum(4,5,3));
-        System.out.println(genericsFindMax.findMaximum(1.5,2.5,3.5));
-        System.out.println(genericsFindMax.findMaximum("apple", "peach", "banana"));
+        System.out.println(genericsFindMax.findMaximum(4,5,3,6,10));
+        System.out.println(genericsFindMax.findMaximum(1.5,2.5,3.5,3.7));
+        System.out.println(genericsFindMax.findMaximum("apple", "peach", "banana","orange"));
     }
 }
